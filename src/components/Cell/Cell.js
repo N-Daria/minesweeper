d7c7a049970adf4fdf49b7966e078a9efe198b9b
Daria Nikulina <daryanickulina@yandex.ru>
2023-03-04 17:43:41 +0400

    feat: adds cellmarking with right click

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,12 +1,33 @@
 import './Cell.css';
 import React from 'react';
 
-export default React.memo(() => {
-  // const changeCardLike = () => {};
+export default React.memo(({ startGame }) => {
+  const handleClick = (event) => {
+    console.log(event);
+    startGame();
+  };
+
+  const markCell = (event) => {
+    event.preventDefault();
+
+    if (event.target.textContent === '') {
+      event.target.textContent = '🚩';
+    } else if (event.target.textContent === '🚩') {
+      event.target.textContent = '❓';
+    } else if (event.target.textContent === '❓') {
+      event.target.textContent = '';
+    }
+  };
 
   return (
     <li className="cell">
-      <button type="button" className="cell__button" aria-label="chose the cell" />
+      <button
+        type="button"
+        className="cell__button"
+        aria-label="chose the cell"
+        onClick={handleClick}
+        onContextMenu={markCell}
+      />
     </li>
   );
 });
